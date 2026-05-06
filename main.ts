761e1788916879c184a15b3d9164367656120525
Yannick Boogaerts <yboogaerts@gmail.com>
2026-05-06 15:38:31 +0200

    possibilité d'ajout d'action a chaque étape

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 namespace Parcours{
+   type Action = (userPos:number, serialNumber:number, message: string)=> void;
    let user: string[] = [] 
    let userId: number[] = [] 
-   let userState: number[]=[]
+   let userState: number[] = []
+   let stateAction: Action[] = []
 
+    //% block="Action à l'étape $etape"
+    export function setAction(pos : number, action : Action){
+        stateAction[pos] = action
+    }
+
+    radio.onReceivedString(function (receivedString: string) {
+        let serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        let pos = userId.indexOf(serial )
+        let state = 0;
+        if( pos >= 0 ){
+            state = userState[pos];
+        }
+
+        // On vérifie si un handler existe pour l'état actuel
+        if (stateAction[pos]) {
+            // On exécute le handler en lui passant les données "capturées"
+            stateAction[pos](pos, serial, receivedString)
+        }
+    })
     //% block="inscrire $nom avec le numéro $id"
     export function inscription(nom: string, id: number) {
         let pos = userId.indexOf(id)
@@ -17,9 +38,9 @@ namespace Parcours{
 
     //% block="étape suivante pour $id"
     export function nextState(id: number) {
-        let pos = userId.indexOf(id)
-        if (pos >= 0) {
-            userState[pos]++
+        let pos2 = userId.indexOf(id)
+        if (pos2 >= 0) {
+            userState[pos2]++
         }
     }
 
@@ -30,4 +51,3 @@ namespace Parcours{
         }
     }
     }
-   
