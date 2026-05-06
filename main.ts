5b476d931c264a07e1fa2760f7589bd736ff0bf4
Yannick boogaerts <yboogaerts@gmail.com>
2026-05-06 11:02:54 +0200

    test de console

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 namespace MonOutil {
+    let User: string[]=[] 
     //% block="dire bonjour"
     export function direBonjour() {
         basic.showString("Salut !")
     }
+    
     //% block="dire auRevoir"
     export function direauRevoir() {
         basic.showString("By !")
-    }
+        console.log("test")
 }
+    }
+   
