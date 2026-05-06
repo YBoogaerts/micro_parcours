ac3b5ed4dd0ef1db28ffff63cef9f63bc9a324b0
Yannick Boogaerts <yboogaerts@gmail.com>
2026-05-06 13:21:15 +0200

    ajout des étapes du joueur

--- main.ts
+++ main.ts
@@ -1,22 +1,32 @@
 namespace Parcours{
    let user: string[] = [] 
-   let idUser: number[] = [] 
-    
+   let userId: number[] = [] 
+   let userState: number[]=[]
+
     //% block="inscrire $nom avec le numéro $id"
     export function inscription(nom: string, id: number) {
-        let pos = idUser.indexOf(id)
+        let pos = userId.indexOf(id)
         if (pos < 0) {
             user.push(nom)
-            idUser.push(id) 
+            userId.push(id)
+            userState.push(0)
         } else {
-            user[pos]= nom
+            user[pos] = nom
         }
     }
-    
+
+    //% block="étape suivante pour $id"
+    export function nextState(id: number) {
+        let pos = userId.indexOf(id)
+        if (pos < 0) {
+            userState[pos]++
+        }
+    }
+
     //% block="affiche les joueurs en console"
     export function printUser() {
-        for (let i = 0; i < idUser.length ; i++) {
-            console.log(`${i} [${idUser[i]}]: ${user[i]}`);
+        for (let i = 0; i < userId.length ; i++) {
+            console.log(`${i} [${userId[i]}]: ${user[i]} -> étape: ${userState[i]}`);
         }
     }
     }
