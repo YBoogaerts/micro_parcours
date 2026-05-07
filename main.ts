78ede353bffdf6e1de31942a7f33ff23ad590f09
Yannick Boogaerts <yboogaerts@gmail.com>
2026-05-07 09:13:09 +0200

    Modification du nom des variables

--- main.ts
+++ main.ts
@@ -1,61 +1,61 @@
 namespace Parcours{
-   type Action = (userPos:number, serialNumber:number, message: string)=> void;
-   let user: string[] = [] 
-   let userId: number[] = [] 
-   let userState: number[] = []
-   let stateAction: Action[] = []
+   type Action = (userId:number, serialNumber:number, message: string)=> void;
+   let userList: string[] = [] 
+   let userIdList: number[] = [] 
+   let userStateList: number[] = []
+   let stateActionList: Action[] = []
 
     /**
      * Définit l'action à exécuter pour une étape précise
-     * @param pos l'indice de l'étape
+     * @param userId le numéro de l'utilisateur
      * @param action le code à exécuter
      */
-    //% block="Action à l'étape $pos"
+    //% block="Action à l'étape $userId"
     //% handlerStatement=1
     //% draggableParameters="reporter"
-    export function setAction(pos: number, action: (userPos: number, serialNumber: number, message: string) => void) {
-        stateAction[pos] = action
-        console.log(action)
+    export function setAction(userId: number, action: (userId: number, serialNumber: number, message: string) => void) {
+        stateActionList[userId] = action
+        //console.log(action)
     }
     radio.onReceivedString(function (receivedString: string) {
+        // id de l'émetteur
         let serial = radio.receivedPacket(RadioPacketProperty.SerialNumber)
-        let pos = userId.indexOf(serial )
+        // numéro d'utilisateur
+        let userId = userIdList.indexOf(serial )
+        // calcul de l'étape de l'utilisateur
         let state = 0;
-        console.log(receivedString)
-        if( pos >= 0 ){
-            state = userState[pos];
+        // console.log(receivedString)
+        if (userId >= 0 ){
+            state = userStateList[userId];
         }
 
         // On vérifie si un handler existe pour l'état actuel
-        if (stateAction[pos]) {
+        if (stateActionList[userId]) {
             // On exécute le handler en lui passant les données "capturées"
-            stateAction[pos](pos, serial, receivedString)
+            stateActionList[userId](userId, serial, receivedString)
         }
     })
     //% block="inscrire $nom avec le numéro $id"
     export function inscription(nom: string, id: number) {
-        let pos = userId.indexOf(id)
-        if (pos < 0) {
-            user.push(nom)
-            userId.push(id)
-            userState.push(0)
+        let userId = userIdList.indexOf(id)
+        if (userId < 0) {
+            userList.push(nom)
+            userIdList.push(id)
+            userStateList.push(0)
         } else {
-            user[pos] = nom
+            userList[userId] = nom
         }
     }
 
-    //% block="étape suivante pour $id"
-    export function nextState(id: number) {
-        let pos2 = userId.indexOf(id)
-        if (pos2 >= 0) {
-            userState[pos2]++
-        }
+    //% block="étape suivante pour $userId"
+    export function nextState(userId: number) {
+        userStateList[userId]++
     }
 
     //% block="affiche les joueurs en console"
     export function printUser() {
-        for (let i = 0; i < userId.length ; i++) {
-            console.log(`${i} [${userId[i]}]: ${user[i]} -> étape: ${userState[i]}`);
+        for (let i = 0; i < userIdList.length ; i++) {
+            console.log(`${i} [${userIdList[i]}]: ${userList[i]} -> étape: ${userStateList[i]}`);
         }
     }
     }
